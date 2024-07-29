8fdca9d718f090678456cb8195c5f1adf74bbdb0
Ville Lähetkangas <lahetkan@protonmail.com>
2024-07-29 17:40:52 +0300

    Prep for publish

--- backend/dist/backend/src/index.js
+++ backend/dist/backend/src/index.js
@@ -8,15 +8,24 @@ const cors_1 = __importDefault(require("cors"));
 const product_router_1 = __importDefault(require("./routers/product.router"));
 const quest_router_1 = __importDefault(require("./routers/quest.router"));
 const achievement_router_1 = __importDefault(require("./routers/achievement.router"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
 const PORT = process.env.PORT || 8080;
+app.use((0, cors_1.default)());
 // Middleware to parse JSON
 app.use(express_1.default.json());
+//Serve images
 app.use(express_1.default.static("resources/images/"));
+// Serve client
+app.use(express_1.default.static(path_1.default.join(__dirname, "..", "..", "client/build")));
+//Routers
 app.use("/api/product", product_router_1.default);
 app.use("/api/quest", quest_router_1.default);
 app.use("/api/achievement", achievement_router_1.default);
+//As default send back client index.html
+app.get("*", (_, res) => {
+    res.sendFile(path_1.default.join(__dirname, "..", "..", "client/build", "index.html"));
+});
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,19 +3,27 @@ import cors from "cors";
 import productRouter from "./routers/product.router";
 import questRouter from "./routers/quest.router";
 import achievementRouter from "./routers/achievement.router";
+import path from "path";
 
 const app = express();
-app.use(cors());
 const PORT = process.env.PORT || 8080;
+app.use(cors());
 
 // Middleware to parse JSON
 app.use(express.json());
+//Serve images
 app.use(express.static("resources/images/"));
+// Serve client
+app.use(express.static(path.join(__dirname, "..", "..", "client/build")));
 
+//Routers
 app.use("/api/product", productRouter);
 app.use("/api/quest", questRouter);
 app.use("/api/achievement", achievementRouter);
-
+//As default send back client index.html
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "client/build", "index.html"));
+});
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
